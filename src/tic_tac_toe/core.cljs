d9b1291a04551a5cf945cefdd197320b48d0f63c
terry cadd <terry.cadd@gmail.com>
2019-08-06 12:32:30 +0100

    clicking svg works , but doesnt clear svg once they are there.

--- src/tic_tac_toe/core.cljs
+++ src/tic_tac_toe/core.cljs
@@ -12,7 +12,9 @@
 (enable-console-print!)
 
 
-(def board-state (atom {:sq [0,0,0,0,0,0,0,0,0] :who-go 1 }))
+(def title (atom "Tic Tac Toe"))
+
+(def board-state (reagent/atom {:sq [0,0,0,0,0,0,0,0,0] :who-go 1 :winner false}))
 
 ;; recap javascript mouse events 
 ;; function clickEvent(e) {
@@ -21,6 +23,40 @@
 ;;   var x = e.clientX - rect.left; //x position within the element.
 ;;   var y = e.clientY - rect.top;  //y position within the element.
 ;; }
+
+
+;; (list
+;;  [0 1 2]
+;;  [3 4 5]
+;;  [6 7 8]
+;;  [0 3 6]
+;;  [1 4 7]
+;;  [2 5 8]
+;;  [0 4 8]
+;;  [2 4 6])
+
+(defn check-for-winner [sq a b c]
+  (and (not (= (sq a) 0))
+       (= (sq a) (sq b) (sq c))))
+
+
+(defn any-winners [sq]
+  
+   false
+    
+;; 0 1 2
+;; 3 4 5
+;; 6 7 8
+;; 0 3 6
+;; 1 4 7
+;; 2 5 8
+;; 0 4 8
+;; 2 4 6
+  
+  
+  )
+
+
 
 
 (defn you-clicked [ev]
@@ -51,40 +87,54 @@
     1))
 
 
+(defn make-square-clickable [x y s bgcolor sq player]  
+  (list
+   [:rect {:x x
+           :y y
+           :width "1"
+           :height "1"
+           :style {:fill bgcolor}
+           :on-click (fn [ev]
+                       (reset! board-state {:sq (assoc sq s player)  :who-go (next-player player)})
+                       (console.log "you clicked on my square x,y= " x " , " y )
+                       )
+           }]
+   ))
 
-(defn make-square [x y s bgcolour]
+
+
+(defn make-square-occupied [x y s bgcolor player]
+  (list
+   [:rect {:x x
+           :y y
+           :width "1"
+           :height "1"
+           :style {:fill bgcolor}           
+           }]
+   ;; draw a circle if player = 1 of certain color
+   ;; draw a circle if player = 2 of different color   
+   [:circle {:cx (+ x 0.5) :cy (+ y 0.5) :r 0.45 :style {:stroke "green" :stroke-width "0.01" :fill
+                                                        (if (= player 1)
+                                                          "yellow"
+                                                          "black")}} ]
+   ))
+
+
+(defn make-square-player-1 [x y s bgcolor]
+  (make-square-occupied x y s bgcolor 1))
+
+(defn make-square-player-2 [x y s bgcolor]
+  (make-square-occupied x y s bgcolor 2))
+
+
+(defn make-square [x y s bgcolor]
   (let [sq (get @board-state :sq)]
     (let [player (get @board-state :who-go)]
       (cond
         ;; its an empty square - when clicked places a 1 or a 2 at that location of board-state
-        (= (sq s) 0)
-        [:rect {:x x
-                :y y
-                :width "1"
-                :height "1"
-                :style {:fill bgcolour}
-                :on-click #(reset! board-state {:sq (assoc sq s player)  :who-go (next-player player)})
-                }]
-
-        ;; its an occupied square by player 1
-        (= (sq s) 1)
-        [:rect {:x x
-                :y y
-                :width "1"
-                :height "1"
-                :style {:fill bgcolour}
-                }]
-        
-        ;; its an occupied square by player 2
-        (= (sq s) 2)
-        [:rect {:x x
-                :y y
-                :width "1"
-                :height "1"
-                :style {:fill bgcolour}
-                }]
-        ))))
-
+        (= (sq s) 0) (make-square-clickable x y s bgcolor sq player)
+        (= (sq s) 1) (make-square-player-1 x y s bgcolor)
+        true         (make-square-player-2 x y s bgcolor)))))
 
 
               
@@ -114,47 +164,85 @@
 
 
 (defn four-lines []
-  (for [x [1 2 3 4]]
-    :let [y
-          (if (= x 1)
-            [line-1]
-            (if (= x 2)
-              [line-2]
-              (if (= x 3)
-                [line-3]
-                [line-4])))]
-    y))
+  (list
+   [line-1]
+   [line-2]
+   [line-3]
+   [line-4]))
+
+(defn lots-horizontal-lines []
+  (into
+   []
+   (for [y (range -0.01 3.3 0.2)]
+    [:rect {:x "0"
+            :y y
+            :width "3"
+            :height "0.01"
+            :style {:fill "black"}}])))
+
+
+(defn lots-vertical-lines []
+  (into
+   []
+   (for [x (range -0.01 3.3 0.2)]
+    [:rect {:x x
+            :y "0"
+            :width "0.01"
+            :height "3"
+            :style {:fill "black"}}])))
+
+
+;; 
+;;
+;; dev:cljs.user=> (into [1 2 3] '(:a :b))
+;; [1 2 3 :a :b]
+;; dev:cljs.user=> (into (into [1 2 3] '(:a :b)) '(:c :d))
+;; [1 2 3 :a :b :c :d]
+;; dev:cljs.user=> (into (into (into [1 2 3] '(:a :b)) '(:c :d)) '(:e :f))
+;; [1 2 3 :a :b :c :d :e :f]
+;; dev:cljs.user=> (into (into (into (into [1 2 3] '(:a :b)) '(:c :d)) '(:e :f)) '(:g :h))
+;; [1 2 3 :a :b :c :d :e :f :g :h]
 
 
 
 ;;[make-square 0 0 0 "blue"]
 (defn tic-tac-toe-board []
-  [:div {:class "game"}
-   [:svg {:width "500" :height "500" :viewBox "0 0 3 3" :style {:margin "none"}}
+  [:div {:class "game" :align "center"}
+   (reduce into
+           [:svg {:width "500" :height "500" :viewBox "0 0 3 3" :style {:margin "none"}}]           
+           (list
 
-    [four-lines]
-    
-    ;; (let [r [:rect {:x "0"  :y "1"  :width "3" :height "0.01" :style {:fill "black"}}]]
-    ;;   (console.log "rect = " r)
-    ;;   r)
-    
-    ]
-   ]
-  )
+            (make-square 0 0 0 "blue")
+            (make-square 1 0 1 "green")
+            (make-square 2 0 2 "blue")
+            
+            (make-square 0 1 3 "green")
+            (make-square 1 1 4 "blue")
+            (make-square 2 1 5 "green")
+
+            (make-square 0 2 6 "blue")
+            (make-square 1 2 7 "green")
+            (make-square 2 2 8 "blue")
+
+            (four-lines)
+            
+            ))])
 
 
-
+     ;; (make-square 1 0 1 "green")
+     ;; (make-square 2 0 2 "blue")
+     
+     ;; (make-square 0 1 3 "green")
+     ;; (make-square 1 1 4 "blue")
+     ;; (make-square 2 1 5 "green")
+     
+     ;; (make-square 0 2 6 "blue")
+     ;; (make-square 1 2 7 "green")
+     ;; (make-square 2 2 8 "blue")
+     
 
     ;;[lines]
-     
-  
 
-
-
-
-
-     
-    
     ;; [:rect {:x "1"  :y "0" :width "1" :height "1" :style {:fill "green"}  :on-click (fn [ev] (console.log "square 2 clicked" )) } ]
     ;; [:rect {:x "2"  :y "0" :width "1" :height "1" :style {:fill "blue"}  :on-click (fn [ev] (console.log "square 3 clicked" )) } ]
 
@@ -192,27 +280,27 @@
     ;;             :on-mouse-over #(console.log "mouse over!! x = ")
     ;;             } ])
 
-
+ 
   
   
-  
-  
-  
-  
-
-
-
-
+(defn new-game []
+  (reset! board-state (reagent/atom {:sq [0,0,0,0,0,0,0,0,0] :who-go 1 :winner false})))
 
 (defn tic-tac-toe []
   [:div {:class "clock"}
-   [:h1 {:style {:text-align "center" } } "Tic-Tac-Toe App"]
+   [:h1 {:style {:text-align "center" } } @title]
    
    [tic-tac-toe-board]
-   ;;[:img {:id "img1"           :src "img1.jpg"           :style {:visibility "shown" :opacity @opacity}}] 
-   [:div {:id "div2"}
-    ;;[:img {:id "img2"            :src "img2.jpg"            :style {:visibility "shown"}}]
+   [:div {:id "newGame" :align "center"}
+    [:button {:on-click (fn [ev]
+                          [new-game]
+                          (console.log "should be a new game !")
+                          )
+              } "New Game" ]
     ]
+   
+   
+   ;;[:img {:id "img1"           :src "img1.jpg"           :style {:visibility "shown" :opacity @opacity}}] 
    [:div {:id "div3"}
     ;;[:img {:id "img3" :src "img3.jpg" :style {:visibility "shown"}}]
     ]
